c511aa061a0dd8ec5b4737933c4a690be92e64aa
sbansal05 <siddhi.bansal0504@gmail.com>
2026-02-11 17:02:42 +0530

    wallet generation done
    Reset Solana and Ethereum URLs to empty strings.

--- src/conf/config.ts
+++ src/conf/config.ts
@@ -1,2 +1,2 @@
-export const solanaUrl = "https://solana-devnet.g.alchemy.com/v2/rjOaDuhdBO1Uim1Sp3bFe";
-export const ethUrl = "https://eth-sepolia.g.alchemy.com/v2/rjOaDuhdBO1Uim1Sp3bFe";
+export const solanaUrl = "";
+export const ethUrl = "";
